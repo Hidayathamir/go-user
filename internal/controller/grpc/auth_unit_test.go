227abf9149485fe7dc8cbf9c5633b3c3919df8ca
Hidayat Hamir <hidayat.03@erajaya.com>
2024-03-23 03:43:03 +0700

    test: grpc unit test

---
+++ internal/controller/grpc/auth_unit_test.go
@@ -0,0 +1,144 @@
+package grpc
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Hidayathamir/go-user/config"
+	"github.com/Hidayathamir/go-user/internal/dto"
+	"github.com/Hidayathamir/go-user/internal/usecase/mockusecase"
+	"github.com/Hidayathamir/go-user/pkg/gousergrpc"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func TestUnitAuthLoginUser(t *testing.T) {
+	t.Parallel()
+
+	gin.SetMode(gin.TestMode)
+
+	t.Run("call usecase LoginUser success should return success", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		usecaseAuth := mockusecase.NewMockIAuth(ctrl)
+
+		a := &Auth{
+			cfg:         config.Config{},
+			usecaseAuth: usecaseAuth,
+		}
+
+		usecaseAuth.EXPECT().LoginUser(gomock.Any(), dto.ReqLoginUser{
+			Username: "hidayat",
+			Password: "mypassword",
+		}).Return("Bearer dummyUserJWT", nil)
+
+		req := &gousergrpc.ReqLoginUser{
+			Username: "hidayat",
+			Password: "mypassword",
+		}
+
+		res, err := a.LoginUser(context.Background(), req)
+
+		assert.NotNil(t, res.GetUserJwt())
+		assert.Contains(t, res.GetUserJwt(), "dummyUserJWT")
+		require.NoError(t, err)
+	})
+	t.Run("call usecase LoginUser error should return error", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		usecaseAuth := mockusecase.NewMockIAuth(ctrl)
+
+		a := &Auth{
+			cfg:         config.Config{},
+			usecaseAuth: usecaseAuth,
+		}
+
+		usecaseAuth.EXPECT().LoginUser(gomock.Any(), dto.ReqLoginUser{
+			Username: "hidayat",
+			Password: "mypassword",
+		}).Return("", assert.AnError)
+
+		req := &gousergrpc.ReqLoginUser{
+			Username: "hidayat",
+			Password: "mypassword",
+		}
+
+		res, err := a.LoginUser(context.Background(), req)
+
+		assert.Nil(t, res)
+		require.Error(t, err)
+		require.ErrorIs(t, err, assert.AnError)
+	})
+}
+
+func TestUnitAuthRegisterUser(t *testing.T) {
+	t.Parallel()
+
+	gin.SetMode(gin.TestMode)
+
+	t.Run("call usecase RegisterUser success should return success", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		usecaseAuth := mockusecase.NewMockIAuth(ctrl)
+
+		a := &Auth{
+			cfg:         config.Config{},
+			usecaseAuth: usecaseAuth,
+		}
+
+		usecaseAuth.EXPECT().RegisterUser(gomock.Any(), dto.ReqRegisterUser{
+			Username: "hidayat",
+			Password: "mypassword",
+		}).Return(int64(323), nil)
+
+		req := &gousergrpc.ReqRegisterUser{
+			Username: "hidayat",
+			Password: "mypassword",
+		}
+
+		res, err := a.RegisterUser(context.Background(), req)
+
+		assert.Equal(t, int64(323), res.GetUserId())
+		require.NoError(t, err)
+	})
+	t.Run("call usecase RegisterUser error should return error", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		usecaseAuth := mockusecase.NewMockIAuth(ctrl)
+
+		a := &Auth{
+			cfg:         config.Config{},
+			usecaseAuth: usecaseAuth,
+		}
+
+		usecaseAuth.EXPECT().RegisterUser(gomock.Any(), dto.ReqRegisterUser{
+			Username: "hidayat",
+			Password: "mypassword",
+		}).Return(int64(0), assert.AnError)
+
+		req := &gousergrpc.ReqRegisterUser{
+			Username: "hidayat",
+			Password: "mypassword",
+		}
+
+		res, err := a.RegisterUser(context.Background(), req)
+
+		assert.Nil(t, res)
+		require.Error(t, err)
+		require.ErrorIs(t, err, assert.AnError)
+	})
+}
---
+++ internal/controller/grpc/profile_unit_test.go
@@ -0,0 +1,155 @@
+package grpc
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/Hidayathamir/go-user/config"
+	"github.com/Hidayathamir/go-user/internal/dto"
+	"github.com/Hidayathamir/go-user/internal/usecase/mockusecase"
+	"github.com/Hidayathamir/go-user/pkg/gousergrpc"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func TestUnitProfileGetProfileByUsername(t *testing.T) {
+	t.Parallel()
+
+	gin.SetMode(gin.TestMode)
+
+	t.Run("call usecase GetProfileByUsername success should return success", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		usecaseProfile := mockusecase.NewMockIProfile(ctrl)
+
+		p := &Profile{
+			cfg:            config.Config{},
+			usecaseProfile: usecaseProfile,
+		}
+
+		user := dto.ResGetProfileByUsername{
+			ID:        23,
+			Username:  "hidayat",
+			CreatedAt: time.Time{},
+			UpdatedAt: time.Time{},
+		}
+
+		usecaseProfile.EXPECT().
+			GetProfileByUsername(gomock.Any(), "hidayat").
+			Return(user, nil)
+
+		req := &gousergrpc.ReqGetProfileByUsername{
+			Username: "hidayat",
+		}
+
+		res, err := p.GetProfileByUsername(context.Background(), req)
+
+		assert.NotNil(t, res)
+		assert.Equal(t, user.ID, res.GetId())
+		assert.Equal(t, user.Username, res.GetUsername())
+		assert.Equal(t, user.CreatedAt, res.GetCreatedAt().AsTime())
+		assert.Equal(t, user.UpdatedAt, res.GetUpdatedAt().AsTime())
+		require.NoError(t, err)
+	})
+	t.Run("call usecase GetProfileByUsername error should return error", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		usecaseProfile := mockusecase.NewMockIProfile(ctrl)
+
+		p := &Profile{
+			cfg:            config.Config{},
+			usecaseProfile: usecaseProfile,
+		}
+
+		user := dto.ResGetProfileByUsername{}
+
+		usecaseProfile.EXPECT().
+			GetProfileByUsername(gomock.Any(), "hidayat").
+			Return(user, assert.AnError)
+
+		req := &gousergrpc.ReqGetProfileByUsername{
+			Username: "hidayat",
+		}
+
+		res, err := p.GetProfileByUsername(context.Background(), req)
+
+		assert.Nil(t, res)
+		require.Error(t, err)
+		require.ErrorIs(t, err, assert.AnError)
+	})
+}
+
+func TestProfileUpdateProfileByUserID(t *testing.T) {
+	t.Parallel()
+
+	gin.SetMode(gin.TestMode)
+
+	t.Run("call usecase UpdateProfileByUserID success should return success", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		usecaseProfile := mockusecase.NewMockIProfile(ctrl)
+
+		p := &Profile{
+			cfg:            config.Config{},
+			usecaseProfile: usecaseProfile,
+		}
+
+		usecaseProfile.EXPECT().
+			UpdateProfileByUserID(gomock.Any(), dto.ReqUpdateProfileByUserID{
+				UserJWT:  "Bearer dummyUserJWT",
+				Password: "newpassword",
+			}).Return(nil)
+
+		req := &gousergrpc.ReqUpdateProfileByUserID{
+			UserJwt:  "Bearer dummyUserJWT",
+			Password: "newpassword",
+		}
+
+		res, err := p.UpdateProfileByUserID(context.Background(), req)
+
+		assert.NotNil(t, res)
+		require.NoError(t, err)
+	})
+	t.Run("call usecase UpdateProfileByUserID error should return error", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		usecaseProfile := mockusecase.NewMockIProfile(ctrl)
+
+		p := &Profile{
+			cfg:            config.Config{},
+			usecaseProfile: usecaseProfile,
+		}
+
+		usecaseProfile.EXPECT().
+			UpdateProfileByUserID(gomock.Any(), dto.ReqUpdateProfileByUserID{
+				UserJWT:  "Bearer dummyUserJWT",
+				Password: "newpassword",
+			}).Return(assert.AnError)
+
+		req := &gousergrpc.ReqUpdateProfileByUserID{
+			UserJwt:  "Bearer dummyUserJWT",
+			Password: "newpassword",
+		}
+
+		res, err := p.UpdateProfileByUserID(context.Background(), req)
+
+		assert.Nil(t, res)
+		require.Error(t, err)
+		require.ErrorIs(t, err, assert.AnError)
+	})
+}
