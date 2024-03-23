905a4df269fd6d60e15b49f1aab1024352df9d92
Hidayat Hamir <hidayat.03@erajaya.com>
2024-03-23 13:37:33 +0700

    test: grpc integration auth profile update profile

--- internal/controller/grpc/profile_integration_test.go
+++ internal/controller/grpc/profile_integration_test.go
@@ -14,6 +14,128 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestIntegrationProfileUpdateProfileByUserID(t *testing.T) {
+	t.Parallel()
+
+	t.Run("registered user update profile should success", func(t *testing.T) {
+		t.Parallel()
+
+		cfg := initTestIntegration(t)
+
+		pg, err := db.NewPostgresPoolConnection(cfg)
+		require.NoError(t, err)
+
+		repoAuth := repo.NewAuth(cfg, pg)
+		repoProfile := repo.NewProfile(cfg, pg)
+		usecaseAuth := usecase.NewAuth(cfg, repoAuth, repoProfile)
+		controllerAuth := newAuth(cfg, usecaseAuth)
+
+		usecaseProfile := usecase.NewProfile(cfg, repoProfile)
+		controllerProfile := newProfile(cfg, usecaseProfile)
+
+		username := uuid.NewString()
+		oldPassword := uuid.NewString()
+		resRegister, err := controllerAuth.RegisterUser(context.Background(), &gousergrpc.ReqRegisterUser{
+			Username: username,
+			Password: oldPassword,
+		})
+		assert.NotNil(t, resRegister)
+		require.NoError(t, err)
+
+		resLogin, err := controllerAuth.LoginUser(context.Background(), &gousergrpc.ReqLoginUser{
+			Username: username,
+			Password: oldPassword,
+		})
+		assert.NotNil(t, resLogin)
+		require.NoError(t, err)
+
+		newPassword := uuid.NewString()
+		resUpdate, err := controllerProfile.UpdateProfileByUserID(context.Background(), &gousergrpc.ReqUpdateProfileByUserID{
+			UserJwt:  resLogin.GetUserJwt(),
+			Password: newPassword,
+		})
+		assert.NotNil(t, resUpdate)
+		require.NoError(t, err)
+
+		t.Run("after update profile should able to login with new password", func(t *testing.T) {
+			resLogin, err = controllerAuth.LoginUser(context.Background(), &gousergrpc.ReqLoginUser{
+				Username: username,
+				Password: newPassword,
+			})
+			assert.NotNil(t, resLogin)
+			require.NoError(t, err)
+		})
+		t.Run("after update profile should not able to login with old password", func(t *testing.T) {
+			resLogin, err = controllerAuth.LoginUser(context.Background(), &gousergrpc.ReqLoginUser{
+				Username: username,
+				Password: oldPassword,
+			})
+			assert.Nil(t, resLogin)
+			require.Error(t, err)
+			require.ErrorIs(t, err, gouser.ErrWrongPassword)
+		})
+	})
+	t.Run("update profile but request invalid should error", func(t *testing.T) {
+		t.Parallel()
+
+		cfg := initTestIntegration(t)
+
+		pg, err := db.NewPostgresPoolConnection(cfg)
+		require.NoError(t, err)
+
+		repoProfile := repo.NewProfile(cfg, pg)
+		usecaseProfile := usecase.NewProfile(cfg, repoProfile)
+		controllerProfile := newProfile(cfg, usecaseProfile)
+
+		t.Run("request user jwt empty should error", func(t *testing.T) {
+			res, err := controllerProfile.UpdateProfileByUserID(context.Background(), &gousergrpc.ReqUpdateProfileByUserID{
+				Password: uuid.NewString(),
+			})
+			assert.Nil(t, res)
+			require.Error(t, err)
+			require.ErrorIs(t, err, gouser.ErrRequestInvalid)
+		})
+		t.Run("request header user jwt wrong should error", func(t *testing.T) {
+			res, err := controllerProfile.UpdateProfileByUserID(context.Background(), &gousergrpc.ReqUpdateProfileByUserID{
+				UserJwt:  "sdf",
+				Password: uuid.NewString(),
+			})
+			assert.Nil(t, res)
+			require.Error(t, err)
+			require.ErrorIs(t, err, gouser.ErrJWTAuth)
+		})
+		t.Run("request password empty should error", func(t *testing.T) {
+			repoAuth := repo.NewAuth(cfg, pg)
+			usecaseAuth := usecase.NewAuth(cfg, repoAuth, repoProfile)
+			controllerAuth := newAuth(cfg, usecaseAuth)
+
+			username := uuid.NewString()
+			password := uuid.NewString()
+
+			resRegister, err := controllerAuth.RegisterUser(context.Background(), &gousergrpc.ReqRegisterUser{
+				Username: username,
+				Password: password,
+			})
+			assert.NotNil(t, resRegister)
+			require.NoError(t, err)
+
+			resLogin, err := controllerAuth.LoginUser(context.Background(), &gousergrpc.ReqLoginUser{
+				Username: username,
+				Password: password,
+			})
+			assert.NotNil(t, resLogin)
+			require.NoError(t, err)
+
+			resUpdate, err := controllerProfile.UpdateProfileByUserID(context.Background(), &gousergrpc.ReqUpdateProfileByUserID{
+				UserJwt: resLogin.GetUserJwt(),
+			})
+			assert.Nil(t, resUpdate)
+			require.Error(t, err)
+			require.ErrorIs(t, err, gouser.ErrNothingToBeUpdate)
+		})
+	})
+}
+
 func TestIntegrationProfileGetProfileByUsername(t *testing.T) {
 	t.Parallel()
 
