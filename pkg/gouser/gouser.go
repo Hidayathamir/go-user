73966074c749c93f37d9e7e4b42bfed3cd45be7f
Hidayat Hamir <hidayat.03@erajaya.com>
2024-03-24 21:03:55 +0700

    refator: move err var to it's own file

--- pkg/gouser/gouser.go
+++ pkg/gouser/gouser.go
@@ -1,19 +1,2 @@
 // Package gouser -.
 package gouser
-
-import "errors"
-
-var (
-	// ErrRequestInvalid occurs when request invalid.
-	ErrRequestInvalid = errors.New("request invalid")
-	// ErrJWTAuth occurs when there is a problem with JWT auth.
-	ErrJWTAuth = errors.New("JWT auth")
-	// ErrNothingToBeUpdate occurs when nothing to be update.
-	ErrNothingToBeUpdate = errors.New("nothing to be update")
-	// ErrWrongPassword occurs when user login with wrong password.
-	ErrWrongPassword = errors.New("wrong password")
-	// ErrDuplicateUsername occurs when register user but username already exists.
-	ErrDuplicateUsername = errors.New("duplicate username")
-	// ErrUnknownUsername occurs when username does not exists.
-	ErrUnknownUsername = errors.New("unknown username")
-)
